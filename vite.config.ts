c5c764e1e41a0e6a88bc6dcc37d32d6bd47c4154
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-19 14:36:26 +0200

    feat: adding dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 
 
 export default defineConfig({
   
-  base: '/myPortifolio.io/', 
+  base: '/', 
   server: {
     port: 3000,
     host: '0.0.0.0',
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
