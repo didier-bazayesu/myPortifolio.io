35cb3f56af8e870190449d501ebd3ae155dea561
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-19 14:50:35 +0200

    fix: error in extension of picture with typescript

--- components/picture.ts
+++ components/picture.ts
@@ -1,4 +1,9 @@
+// 1. Import the actual image files
+import hackertonImgFile from './hackerton-project.jpg';
+import codeImgFile from './coding-project.jpg';
+import didierImgFile from './didier.jpg';
 
-export const hackertonImage :string = "./components/hackerton-project.jpg"
-export const codeImage :string = "./components/coding-project.jpg"
-export const didierImage:string = "./components/didier.jpg"
+// 2. Export them as constants
+export const hackertonImage: string = hackertonImgFile;
+export const codeImage: string = codeImgFile;
+export const didierImage: string = didierImgFile;
