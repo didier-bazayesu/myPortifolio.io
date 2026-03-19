c22d37431f0f494f7747e595e0a960becc872134
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-19 14:07:27 +0200

    feat: creating path of the image

---
+++ components/picture.ts
@@ -0,0 +1,4 @@
+
+export const hackertonImage :string = "./components/hackerton-project.jpg"
+export const codeImage :string = "./components/coding-project.jpg"
+export const didierImage:string = "./components/didier.jpg"
